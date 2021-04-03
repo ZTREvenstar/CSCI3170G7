dbd94c5fa67a26c1cfdf1ea331ea1a6c91d0d9e1
ZTR <ztrevenstar@gmail.com>
2021-04-03 17:52:46 +0800

    2nd commit

---
+++ src/mainHandler.java
@@ -0,0 +1,24 @@
+
+public class mainHandler{
+
+	public static void main(String[] args) {
+	
+	mainHandler myHandler = new mainHandler();
+	
+	
+	while (true)
+	{
+		
+		
+		
+	}
+		
+		
+	}	
+	
+}
+
+
+
+
+
